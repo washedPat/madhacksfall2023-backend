56fc9da48841b1f9ca5b87f30f476ae3844c4b67
Patrick <patrick.brophy2@gmail.com>
2023-11-11 12:45:37 -0600

    Revert "Revert "init commit""
    This reverts commit 458e098a772be449f4da321b6cfa228469995138.

---
+++ dist/controllers/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.indexController = void 0;
+function indexController(req, res) {
+    return res.send("hello");
+}
+exports.indexController = indexController;
