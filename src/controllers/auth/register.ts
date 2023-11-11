02b5c9402a3ed8a268b91d069e87bc9c419a0f25
Patrick <patrick.brophy2@gmail.com>
2023-11-11 14:58:48 -0600

    register flow

--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { z } from "zod";
+import { connectToDB } from "../../util";
 
 const RegisterBody = z.object({
 	username: z.string(),
@@ -8,19 +9,48 @@ const RegisterBody = z.object({
 
 type RegisterBody = z.infer<typeof RegisterBody>;
 
-function registerController(req: Request, res: Response) {
-	const body: RegisterBody = RegisterBody.parse(req.body); 
-	console.log(body);
+const User = z.object({
+	username: z.string(),
+	password: z.string()
+})
 
-	// check if registrant is already a user
-	
-	// insert user otherwise
-	
-	// return OK
-	
-	return res.status(200).json({
-		"message": "OK"
-	})
+type User = z.infer<typeof User>;
+
+async function registerController(req: Request, res: Response) {
+	try {
+		const client = await connectToDB();
+		await client.db("admin").command({ ping: 1 });
+		const body: RegisterBody = RegisterBody.parse(req.body); 
+		console.log(body);
+
+		// check if registrant is already a user
+		
+		const database = client.db("parking-app");
+		const users = database.collection<User>("users");
+
+		const result = await users.findOne({
+			"username": body.username
+		});
+
+		if(result) {
+			return res.status(400).json({
+				"message": "User already exists"
+			})
+		}
+
+		// insert user otherwise
+		await users.insertOne(body);
+		
+		// return OK
+		
+		return res.status(200).json({
+			"message": "OK"
+		})
+	} catch {
+		return res.status(500).json({
+			"message": "Error occurred while registering user"
+		})
+	}
 }
 
 export { registerController } ;
