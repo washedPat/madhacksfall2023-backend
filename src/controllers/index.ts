458e098a772be449f4da321b6cfa228469995138
Patrick <patrick.brophy2@gmail.com>
2023-11-11 12:44:59 -0600

    Revert "init commit"
    This reverts commit 702c7b617bdcfda1338bf38ebd6ebc80978f2a28.

--- src/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-function indexController(req: Request, res: Response){
-	return res.send("hello");
-}
-
-export { indexController };
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { indexController } from "../controllers";
-
-const router = Router();
-
-router.get("/", indexController);
-
-
-export { router };
