df603468f015d6e7b73982841f1395e92850a75d
Michael Roytman <mroytman@wisc.edu>
2023-11-11 14:51:00 -0600

    db conn

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import { router } from "./routes";
+import { connectToDatabase } from "./models/connect";
 
-const app = express();
+async function main() {
+	const app = express();
 
-const port = 8080;
-
-app.use(router);
-
-app.listen(8080, () => {
-	console.log(`Listening on http://localhost:${port}`);
-});
+	const port = 8080;
+	
+	app.use(router);
+	
+	let conn = await connectToDatabase(process.env.DB_CONN_STRING!)
+	let result = await conn.db("admin").command({ ping: 1 });
+	console.log(result)
+	
+	app.listen(8080, () => {
+		console.log(`Listening on http://localhost:${port}`);
+	});
+	
+}
+main()
